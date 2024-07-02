35b8abbc710572cf86105fc1f08fdc333b32d7a9
Sungyoun Kim(Peter) <58387861+Sungyoun-Kim@users.noreply.github.com>
2024-07-02 21:31:15 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 301.33 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,13 +1,12 @@
-def solution(elements):
-    answer = 0
-    ele_len = len(elements)
-    sum_set = set()
-    elements = elements + elements
 
-    i =0
-    while i <ele_len:
-        for j in range(i+1,i+1+ele_len):
-            sum_set.add(sum(elements[i:j]))
-        i+=1
-        
-    return len(sum_set)
+def solution(elements):
+    ll = len(elements)
+    res = set()
+
+    for i in range(ll):
+        ssum = elements[i]
+        res.add(ssum)
+        for j in range(i+1, i+ll):
+            ssum += elements[j%ll]
+            res.add(ssum)
+    return len(res)
