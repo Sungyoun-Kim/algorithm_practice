9f5fb1f9a0947941aa2b2f75a544ed8ae525b2b4
Sungyoun Kim(Peter) <58387861+Sungyoun-Kim@users.noreply.github.com>
2024-07-17 02:07:56 +0900

    [level 2] Title: 행렬의 곱셈, Time: 44.71 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,13 +1,6 @@
-def solution(arr1, arr2):
-    row_len = len(arr1)
-    col_len1 = len(arr1[0])
-    col_len2 = len(arr2[0])
-    
-    answer = [[0 for _ in range(col_len2)] for _ in range(row_len)]
-    
-    for i in range(row_len):
-        for j in range(col_len2):
-            for k in range(col_len1):
-                answer[i][j] += arr1[i][k] * arr2[k][j]
-                
-    return answer
+def productMatrix(A, B):
+    return [[sum(a*b for a, b in zip(A_row,B_col)) for B_col in zip(*B)] for A_row in A]
+
+
+def solution(arr1,arr2):
+    return productMatrix(arr1,arr2)
