bb5a4b30f7e45b5d0e4df38daf341b948478c91d
Sungyoun Kim(Peter) <58387861+Sungyoun-Kim@users.noreply.github.com>
2024-12-13 02:12:13 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 420 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -0,0 +1,36 @@
+import sys
+
+
+def solution():
+    N = int(sys.stdin.readline())
+
+    balloons = list(map(int,sys.stdin.readline().split()))
+    result = []
+    result_len = 0
+    boom = 9999
+    idx = 0
+    move = 0
+    while True:
+        if move==0 and balloons[idx] !=boom:
+            result.append(idx+1)
+            result_len+=1
+            if result_len == N:
+                print(' '.join(str(x) for x in result))
+                return
+            
+            move = balloons[idx]
+            balloons[idx] = boom
+            continue
+        
+        elif move <0:
+            idx -=1
+            if idx <0:
+                idx = N-1
+            if balloons[idx] != boom:
+                move +=1
+        elif move >0:
+            idx = (idx+1)%N
+            if balloons[idx] != boom:
+                move-=1
+            
+solution()
